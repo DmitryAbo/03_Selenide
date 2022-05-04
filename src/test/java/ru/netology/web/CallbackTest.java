ec9146530a3335b1e1342ec36d26c3815ae63bfb
DmitryAbo <AbornevDmitry@gmail.com>
2022-05-05 00:19:52 +0300

    check CI work fail

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.*;
 import com.codeborne.selenide.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,13 +19,49 @@ import static com.codeborne.selenide.Selenide.open;
 public class CallbackTest {
 
     @Test
-    public void shouldTest() {
+    public void shouldSuccessSend() {
         open("http://localhost:9999/");
         $(By.cssSelector(".input__control[type=\"text\"]")).setValue("Дмитрий");
         $(By.cssSelector(".input__control[type=\"tel\"]")).setValue("+79155377834");
         $(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         $(By.cssSelector("button")).click();
         $("[data-test-id=\"order-success\"]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.".trim()));
+
+    }
+
+    @Test
+    public void shouldInvalidNameValidPhone() {
+        open("http://localhost:9999/");
+        $(By.cssSelector(".input__control[type=\"text\"]")).setValue("!Дмитрий");
+        $(By.cssSelector(".input__control[type=\"tel\"]")).setValue("+79155377834");
+        $(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        $(By.cssSelector("button")).click();
+        $(By.cssSelector("[data-test-id=\"name\"]")).shouldHave(cssClass("input_invalid"));
+        $(By.cssSelector("[data-test-id=\"phone\"]")).shouldNotHave(cssClass("input_invalid"));
+
+    }
+
+    @Test
+    public void shouldInvalidNameInvalidPhone() {
+        open("http://localhost:9999/");
+        $(By.cssSelector(".input__control[type=\"text\"]")).setValue("!Дмитрий");
+        $(By.cssSelector(".input__control[type=\"tel\"]")).setValue("+79155Д377834");
+        $(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        $(By.cssSelector("button")).click();
+        $(By.cssSelector("[data-test-id=\"name\"]")).shouldHave(cssClass("input_invalid"));
+        $(By.cssSelector("[data-test-id=\"phone\"]")).shouldNotHave(cssClass("input_invalid"));
+
+    }
+
+    @Test
+    public void shouldValidNameInvalidPhone() {
+        open("http://localhost:9999/");
+        $(By.cssSelector(".input__control[type=\"text\"]")).setValue("!Дмитрий");
+        $(By.cssSelector(".input__control[type=\"tel\"]")).setValue("+79155Д377834");
+        $(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        $(By.cssSelector("button")).click();
+        $(By.cssSelector("[data-test-id=\"name\"]")).shouldNotHave(cssClass("input_invalid"));
+        $(By.cssSelector("[data-test-id=\"phone\"]")).shouldHave(cssClass("input_invalid"));
 
     }
 }
